2b8d353e84e9d5d1ab76227fa3f16a955e330c69
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-18 11:42:47 +0300

    feat: implement the IntBuilder class with the nessary methods

--- inheritance/index.js
+++ inheritance/index.js
@@ -104,3 +104,58 @@ StringBuilder.prototype.sub = function(from, n) {
   this.lazyCalls.push(lazyEval.bind(this));
   return this;
 }
+
+class IntBuilder extends BaseBuilder {
+
+  static random(min, max) {
+    BaseBuilder.checkIntegerInput([min, max]);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  static checkDivisionByZero(arg) {
+    if (!arg) throw new Error('Illegal operation: division by zero');
+  }
+
+  constructor(int = 0) {
+    BaseBuilder.checkIntegerInput([int]);
+    super(int);
+  }
+
+  minus(...args) {
+    const lazyEval = () => {
+      BaseBuilder.checkIntegerInput(args);
+      this.value -= args.reduce((sum, arg) => sum + arg);
+    }
+    this.lazyCalls.push(lazyEval);
+    return this;
+  }
+
+  multiply(multiplier) {
+    const lazyEval = () => {
+      BaseBuilder.checkIntegerInput([multiplier]);
+    	this.value *= multiplier;
+    }
+    this.lazyCalls.push(lazyEval);
+    return this;
+  }
+
+  divide(divider) {
+    const lazyEval = () => {
+      BaseBuilder.checkIntegerInput([divider]);
+      IntBuilder.checkDivisionByZero(divider);
+      this.value = Math.trunc(this.value / divider);
+    }
+    this.lazyCalls.push(lazyEval);
+    return this;
+  }
+
+  mod(divider) {
+    const lazyEval = () => {
+      BaseBuilder.checkIntegerInput([divider]);
+      IntBuilder.checkDivisionByZero(divider);
+      this.value %= divider;
+    }
+    this.lazyCalls.push(lazyEval);
+    return this;
+  }
+}
