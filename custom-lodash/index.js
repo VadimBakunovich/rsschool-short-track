f6b3c7e811cbaf18f38b5e593562fe96050f8e36
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-23 14:04:10 +0300

    feat: implement additional methods arrPush and arrSlice

--- custom-lodash/index.js
+++ custom-lodash/index.js
@@ -1,3 +1,16 @@
 class CustomLodash {
 
+  arrPush = (arr, elem) => arr[arr.length] = elem;
+
+  arrSlice(arr, start, end) {
+    const slicedArr = [];
+    if (end > arr.length) end = arr.length;
+    if (start < 0) start = 0;
+    for (let i = start; i < end; i++) {
+      this.arrPush(slicedArr, arr[i]);
+    }
+    return slicedArr;
+  }
+
+  
 }
