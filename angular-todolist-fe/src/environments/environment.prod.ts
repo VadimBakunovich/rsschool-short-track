ba85f3acb87fbf89aa1506db396e6c60e075554c
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-02-20 15:58:49 +0300

    feat: add API params to environment

--- angular-todolist-fe/src/environments/environment.prod.ts
+++ angular-todolist-fe/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://jsonplaceholder.typicode.com/todos',
+  API_PARAMS_QUERY: '_limit',
+  API_PARAMS_VALUE: '5'
 };
