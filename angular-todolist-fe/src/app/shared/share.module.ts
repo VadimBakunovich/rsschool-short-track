2cf4aebe9c4693f3964287c58e30db20729a074f
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-02-20 16:59:52 +0300

    feat: implement toUpperCase pipe

--- angular-todolist-fe/src/app/shared/share.module.ts
+++ angular-todolist-fe/src/app/shared/share.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToUpperCasePipe } from './pipes/to-upper-case.pipe';
 import { StyleDirective } from './directives/style.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddTodoFormComponent } from './add-todo-form/add-todo-form.component';
 
-
-
-
 @NgModule({
   declarations: [
     StyleDirective,
+    ToUpperCasePipe,
     AddTodoFormComponent
   ],
   imports: [
