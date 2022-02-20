01819f83ca9570621e80fe4abfe448d0c3490ce7
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-02-20 19:12:15 +0300

    refactor: remove unnecessary formsmodule import

--- angular-todolist-fe/src/app/shared/shared.module.ts
+++ angular-todolist-fe/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TodoComponent } from './todo/todo.component';
 import { ToUpperCasePipe } from './pipes/to-upper-case.pipe';
 import { StyleDirective } from './directives/style.directive';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AddTodoFormComponent } from './add-todo-form/add-todo-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,7 +16,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule
   ],
