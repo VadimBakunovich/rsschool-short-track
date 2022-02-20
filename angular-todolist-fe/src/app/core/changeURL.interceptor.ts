edb6e2bebbc2fa9ff392869489dd05a65378696d
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-02-20 16:34:43 +0300

    feat: implement changeURL-interceptor

---
+++ angular-todolist-fe/src/app/core/changeURL.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+
+export class changeUrlInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clone = req.clone({
+      url: `${environment.API_URL}/${req.url}`
+    });
+    return next.handle(clone);
+  }
+}
--- angular-todolist-fe/src/app/core/http.service.ts
+++ angular-todolist-fe/src/app/core/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Todo } from '../share/models';
+import { Todo } from '../shared/models';
 
 @Injectable()
 export class HttpService {
