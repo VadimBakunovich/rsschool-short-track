8cdac2ce67204bfed94c3ae3e365f50560c126af
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-02-20 16:39:27 +0300

    feat: declare services in core module

--- angular-todolist-fe/src/app/core/core.module.ts
+++ angular-todolist-fe/src/app/core/core.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpService } from './http.service';
+import { ChangeIdInterceptor } from './changeID.interceptor';
+import { changeUrlInterceptor } from './changeURL.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [
+    HttpService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: changeUrlInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ChangeIdInterceptor,
+      multi: true
+    }
   ]
 })
-export class CoreModule { }
+export class CoreModule {}
