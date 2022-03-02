6252204c0046d52ebb0a2c3eacbd44c709f04f9a
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-03-02 19:45:42 +0300

    feat: implement get by id

--- todolist-be/src/index.ts
+++ todolist-be/src/index.ts
@@ -3,6 +3,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { connect, MongooseError } from 'mongoose';
+import { TodoModel } from './models';
 
 const { PORT, MONGO_DB } = process.env;
 const app = express();
@@ -10,6 +11,13 @@ const app = express();
 app.use(cors());
 app.use(json());
 app.use(morgan('combined'));
+
+app.get('/todos/:id', (req, res) => {
+  TodoModel
+    .findById(req.params.id)
+    .then(todo => todo ? res.send(todo) : res.status(404).send('Task not found'))
+    .catch((err: MongooseError) => console.error(err));
+});
 
 connect(MONGO_DB)
   .then(() => app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`)))
