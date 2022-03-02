2dfbe81079e548ee53c7165e369be0a581c1c0ed
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-03-02 19:17:32 +0300

    feat: add middleware

--- todolist-be/src/index.ts
+++ todolist-be/src/index.ts
@@ -1,9 +1,15 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import { connect, MongooseError } from 'mongoose';
 
 const { PORT, MONGO_DB } = process.env;
 const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(morgan('combined'));
 
 connect(MONGO_DB)
   .then(() => app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`)))
