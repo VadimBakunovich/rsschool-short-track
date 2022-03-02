c2e327ffa832003fae4c32b1c4021b6de039fa6a
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2022-03-02 19:53:01 +0300

    feat: implement post and patch methods

--- todolist-be/src/index.ts
+++ todolist-be/src/index.ts
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import { connect, MongooseError } from 'mongoose';
 import jsonstream from 'jsonstream';
 import { TodoModel } from './models';
+import { Todo } from './interfaces';
 import { paginationHandler } from './utils';
 
 const { PORT, MONGO_DB } = process.env;
@@ -29,6 +30,19 @@ app.get('/todos', (req, res) => {
       .pipe(jsonstream.stringify())
       .pipe(res)
   } catch(err) { console.error(err); }
+});
+
+app.post('/todos', (req, res) => {
+  new TodoModel(req.body).save()
+    .then(todo => res.send(todo))
+    .catch((err: MongooseError) => console.error(err));
+});
+
+app.patch('/todos/:id', (req, res) => {
+  TodoModel
+    .findByIdAndUpdate(req.params.id, req.body as Todo, { new: true })
+    .then(todo => res.send(todo))
+    .catch((err: MongooseError) => console.error(err));
 });
 
 connect(MONGO_DB)
